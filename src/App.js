db6ea105784c7d509580d1df571db4a0f833f047
eunshinNoh <nes0410@gmail.com>
2023-01-06 22:57:27 -0700

    refs #4 useTitle

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { content, useTabs } from "./components/useTabs";
-
+import {useTitle} from './components/useTitle'
 const App = () => {
-  // useTabs
-  const { currentItem, changeItem } = useTabs(0, content)
+  // useTitle
+  const titleUpdater = useTitle("Loading...")
+  setTimeout(()=> titleUpdater("Home"), 5000)
   return (
     <div className="App">
-      <h2>{currentItem.content}</h2>
-      {content.map((section, index) => (
-        <button onClick={()=> changeItem(index)}>{section.tab}</button>
-      ))
-      }
+      <h1>Hello</h1>
     </div>
   );
 }
