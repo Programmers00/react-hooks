14fab4665b90867ce007ee9f9f55b7a4bf17d91a
eunshinNoh <nes0410@gmail.com>
2023-01-09 22:28:04 -0700

    refs #13 useFullscreen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { useScroll } from "./components/useScroll";
+import { useFullscreen } from "./components/useFullscreen"
 const App = () => {
-  const { y } = useScroll()
+  const onFullScreen = (isFull) => {
+    console.log(isFull ? "Full" : "Not Full")
+  }
+  const {element, triggerFull, exitFull} = useFullscreen(onFullScreen)
   return (
-    <div className="App" style={{height: '200vh'}}>
-      <h1 style={{position: 'fixed', color: y >= 100 ? 'blue' : 'red'}}>Hello</h1>
+    <div className="App">
+      <div ref={ element }>
+      <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt=''/>
+      <div>
+        <button onClick={triggerFull}>Make Fullscreen</button>
+      <button onClick={exitFull}>Exit Fullscreen</button>  
+        </div>
+        </div>
     </div>
   );
 }
